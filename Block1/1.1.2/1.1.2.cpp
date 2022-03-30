875e9804a25b966450274e31b7a6aafa3acb4d2b
Sinforge <vlad.vlasov77@mail.ru>
2022-03-30 23:12:12 +0300

    add second block (1_1_1, 1_1_2, 1_1_3, 1_1_5)

---
+++ Block1/1.1.2/1.1.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main(){        
+	int num, sum;       
+	sum = 0;      
+	cin >> num;     
+	if (num <= 0) {          
+		cout << "n is wrong (" << num << ")";   
+	}       
+	else {           
+		for (int i = 1; i <= num; i++) {        
+			sum = sum + i;               
+		}             
+		cout << "Sum_n = " << sum;    
+	}        return(0);}
---
+++ Block1/Block1/Block1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main(){       
+	int num;        
+	cin >> num;       
+	num = num % 2;      
+	if (num == 0) {            
+		cout << "even";        }      
+	else {              
+		cout << "uneven";     
+	}       
+	return(0);
+}
